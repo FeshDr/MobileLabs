417728f55a8b6a662ef69f7983efbd54274e8bf9
FeshDr <maksimchikov03@mail.ru>
2024-03-26 16:09:52 +0300

    start

---
+++ MobileLab/MobileLab/MobileLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileLabApp.swift
+//  MobileLab
+//
+//  Created by User on 26.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct MobileLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
